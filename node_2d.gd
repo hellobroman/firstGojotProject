a17b9622f6db3d7cc8543ea69edaad3c0a116372
hellobroman <95313762+hellobroman@users.noreply.github.com>
2025-07-23 09:06:18 -0400

    add new code and charachtersprite

--- node_2d.gd
+++ node_2d.gd
@@ -1,4 +1,20 @@
 extends Node2D
-var bitch = 0
-func _ready():
-	print("bitch")
+var delta = 1
+const SPEED = 200
+func _draw():
+	draw_circle(Vector2(100, 100), 50, Color.RED)
+	draw_rect(Rect2(150, 50, 100, 100), Color.BLUE)
+	draw_line(Vector2(50, 200), Vector2(200, 200), Color.GREEN, 3)
+func _process(delta):
+	var direction = Vector2.ZERO
+
+	if Input.is_action_pressed("ui_up"):
+		direction.y -= 1
+	if Input.is_action_pressed("ui_down"):
+		direction.y += 1
+	if Input.is_action_pressed("ui_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("ui_right"):
+		direction.x += 1
+	direction = direction.normalized()
+	position += direction * SPEED * delta
